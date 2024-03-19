242dc0c7821ec3a9aff63c8539cb3a239494c26d
FrCrypticCode <102952930+FrCrypticCode@users.noreply.github.com>
2024-03-19 23:28:56 +0100

    Update main.rs
    Quelques explications sur le modèle, rien d'exceptionnel.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
+// FFI pour appeler des fonctions externes de Windows => Dans ce cas on appelle une fonction légère d'écoute de touche
 #[link(name="user32")]
 extern "stdcall"{
     fn GetAsyncKeyState(vKey:i32)->i16;
 }
-fn main() {
-    
-}
 
+// Construction d'un objet adapté pour s'interfacer et gérer aisément ses touches
 struct Key{
-    code:char,
-    listen: Box<dyn Fn()->bool>,
-    pressed:bool
+    code:char,    // Code touche
+    listen: Box<dyn Fn()->bool>,    // Closure unsafe pour récupérer le status
+    pressed:bool    // Status
 }
 impl Key{
     fn new(c:char)->Key{
@@ -34,8 +33,11 @@ impl Key{
 }
 
 #[cfg(test)]
+// Exemple d'usage pour écouter les entrées générées par un clavier et une souris
+// Le GetAsyncKeyState de Windows ne fait aucune distinction entre les périphériques, il regarde les codes touches
 fn test(){
     let mut keys = [Key::new('X'),Key::new('Z'),Key::new('Q'),Key::new('D'),Key::new('S'),Key::new(0x01 as char),Key::new(0x02 as char)];
+    // Status sert à gérer une pression prolongée sur touche
     let mut status = [false;7];
     loop{
         for (id,k) in keys.iter_mut().enumerate(){
